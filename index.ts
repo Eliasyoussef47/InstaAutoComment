0ab3442178723db3fc5f80d3b497957b4704d4ef
Elias youssef <d-s-a00@hotmail.com>
2020-09-06 23:40:46 +0200

    removed unneeded comments

--- index.ts
+++ index.ts
@@ -9,16 +9,6 @@ ref = require('instagram-id-to-url-segment');
 instagramIdToUrlSegment = ref.instagramIdToUrlSegment;
 urlSegmentToInstagramId = ref.urlSegmentToInstagramId;
 
-//Betty pk 2153673617
-//Elias pk 321269674
-//SyrianShady pk 34084408297
-//logic pk 10532976
-//loren pk 240759746
-//
-//
-
-//Betty feed item id  2282271774947131603_2153673617
-//SyrianShady feed item id 2289673074177870289_34084408297
 util.inspect.defaultOptions.maxArrayLength = null;
 const loopInterval: number = +process.env.LOOP_INTERVAL;
 const trackingUserPk: number = +process.env.TRACKING_USER_PK;
